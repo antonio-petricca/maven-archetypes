b09b7dee781f5099fb5f2c60269541b9d7811884
antonio.petricca <antonio.petricca@gmail.com>
2018-07-27 16:42:43 +0200

    Removed Model and Logic Modules. Moved PU into EJB module.

--- jboss-web-api/src/ejb/src/main/java/com/ap/archetypes/jboss_web_api/ejb/session/SessionBean.java
+++ jboss-web-api/src/ejb/src/main/java/com/ap/archetypes/jboss_web_api/ejb/session/SessionBean.java
@@ -3,12 +3,21 @@ package com.ap.archetypes.jboss_web_api.ejb.session;
 import com.ap.archetypes.jboss_web_api.common.configuration.AppConfiguration;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 
 @Stateless
 @LocalBean
 public class SessionBean implements SessionBeanLocal {
     
+    @PersistenceContext(unitName = "mainPU", type = PersistenceContextType.TRANSACTION)
+    private EntityManager entityManager;
+    
     @Override
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
     public String sayHello() {
         return AppConfiguration.getInstance().HELLO_MESSAGE;
     }
